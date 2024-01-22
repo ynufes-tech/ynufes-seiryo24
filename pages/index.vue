55f810e6b08c9aea6d3efc201089f633d1fc2b65
shuma-I <rns07k@gmail.com>
2024-01-22 10:42:26 +0900

    nuxtタグをdivに置き換えた。

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,7 @@
-<script setup lang="ts">
-
-</script>
+<script setup lang="ts"></script>
 
 <template>
-    <nuxt />
+  <div></div>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
