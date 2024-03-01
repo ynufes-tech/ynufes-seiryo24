58213d93e06b44b9fe98d62a2058e6d27167c450
hinata0629 <hinata749890@icloud.com>
2024-03-01 17:14:55 +0900

    header-componentの作成

---
+++ components/header-page-icon.vue
@@ -0,0 +1,98 @@
+<script lang="ts" setup>
+</script>
+
+<template>
+<div class="header-page-icon">
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" >
+<link href="https://fonts.googleapis.com/css2?family=Zen+Old+Mincho&display=swap" rel="stylesheet">
+
+  <div class="page-icon-wrapper">
+  <NuxtLink class="page-icon" to="/about">
+    <img src="/public/images/pageIcon/about.png">
+    <span class="hover-icon">about</span>
+  </NuxtLink>
+  <NuxtLink class="page-icon" to="/event">
+    <img src="/public/images/pageIcon/event.png">
+    <span class="hover-icon">event</span>
+  </NuxtLink>
+  <NuxtLink class="page-icon" to="/events">
+    <img src="/public/images/pageIcon/events.png">
+    <span class="hover-icon">events</span>
+  </NuxtLink>
+  <NuxtLink class="page-icon" to="/pamphlet">
+    <img src="/public/images/pageIcon/pamphlet.png">
+    <span class="hover-icon">pamphlet</span>
+  </NuxtLink>
+  <NuxtLink class="page-icon" to="/map">
+    <img src="/public/images/pageIcon/map.png">
+    <span class="hover-icon">map</span>
+  </NuxtLink>
+  <NuxtLink class="page-icon" to="/time-schedule">
+    <img src="/public/images/pageIcon/time-schedule.png">
+    <span class="hover-icon">time-schedule</span>
+  </NuxtLink>
+  <NuxtLink class="page-icon" to="/access">
+    <img src="/public/images/pageIcon/access.png">
+    <span class="hover-icon">access</span>
+  </NuxtLink>
+  <NuxtLink class="page-icon" to="/sponsor">
+    <img src="/public/images/pageIcon/sponsors.png">
+    <span class="hover-icon">sponsors</span>
+  </NuxtLink>
+  <NuxtLink class="page-icon" to="/faq">
+    <img src="/public/images/pageIcon/faq.png">
+    <span class="hover-icon">faq</span>
+  </NuxtLink>
+  </div>
+
+</div>
+
+</template>
+
+<style scoped>
+.header-page-icon{
+  display: flex;
+}
+
+.page-icon-wrapper{
+  display: flex;
+  position: fixed;
+  top: 3%;
+  right: 5%;
+  align-items: center;
+  font-family: 'Zen Old Mincho', serif;
+  color: #333333;
+  @media screen and (max-width: 768px){
+    display: none;
+  }
+  .page-icon{
+    position: relative;
+    margin: 0 8px;
+  }
+  .page-icon:hover .hover-icon{
+    display: inline; 
+  }
+  .hover-icon{
+  position: absolute;              
+  display: none;                      
+  padding: 2px;                        
+  background-color: rgba(246, 246, 246, 0.60);   
+  width:14em;                         
+  left : 30%;                          
+  top : 100%;
+  margin-top : 12px;                    
+  font-size: 18px;
+  }
+  .hover-icon:after{
+  border-bottom: 12px solid rgba(246, 246, 246, 0.60); 
+  border-left: 10px solid transparent;    
+  border-right: 10px solid transparent;   
+  top: -12px;                           
+  left : 5%;                            
+  content: "";                    
+  position: absolute;         
+  }
+}
+
+</style>
---
+++ components/header.vue
@@ -0,0 +1,85 @@
+<script lang="ts" setup>
+</script>
+
+<template>
+<div class="header">
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" >
+<link href="https://fonts.googleapis.com/css2?family=Zen+Old+Mincho&display=swap" rel="stylesheet">
+
+  <div class="ynu-icon-wrapper">
+   <NuxtLink class="header-icon" to="/">
+      <img src="/public/images/YNUFES-icon.png">
+      <div class="title">
+        <a class="header-title">清陵祭</a>
+        <a class="header-sub-title">2024</a>
+      </div>
+   </NuxtLink>
+  </div>
+
+</div>
+
+</template>
+
+<style scoped>
+.header{
+  display: flex;
+}
+.ynu-icon-wrapper{
+  width: fit-content;
+  align-items: center;
+  position: fixed;
+  top: 3%;
+  left: 5%;
+  .header-icon{
+    display: flex;
+    >img{
+      margin-right: 20px;
+      @media screen and (max-width: 768px){
+        width: 50px;
+        height: 50px;
+      }
+
+      @media screen and (max-width: 400px){
+        width: 40px;
+        height: 40px;
+      }
+    }
+  }
+  .title{
+    font-family: 'Zen Old Mincho', serif;
+    color: #333333;
+    width: fit-content;
+    margin: 0 auto;
+    text-align: center;
+    display: flex;
+    flex-direction: column;
+    .header-title{
+      font-size: 24px;
+      margin: 0 auto;
+      text-decoration: none;
+      @media screen and (max-width: 768px){
+        font-size: 20px;
+      }
+
+      @media screen and (max-width: 400px){
+        font-size: 16px;
+      }
+    }
+    .header-sub-title{
+      font-size: 16px;
+      margin: 0 auto;
+      text-decoration: none;
+      @media screen and (max-width: 768px){
+        font-size: 12px;
+      }
+
+      @media screen and (max-width: 400px){
+        font-size: 8px;
+      }
+    }
+  }
+}
+
+
+</style>
