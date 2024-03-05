6ea492adcb32b067723a8ac797fe66e8df75d9ce
hinata0629 <hinata749890@icloud.com>
2024-03-05 14:56:03 +0900

    error.vueの試作

---
+++ components/BackToHome.vue
@@ -0,0 +1,26 @@
+<script lang="ts" setup></script>
+
+<template>
+  <div class="back-to-home">
+    <NuxtLink to="/" class="button"> HOME </NuxtLink>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.back-to-home {
+  text-align: center;
+  display: block;
+  margin: 0 auto;
+  position: relative;
+  bottom: 20%;
+}
+.button {
+  font-size: 24px;
+  font-family: "Zen Old Mincho", serif;
+  text-decoration: none;
+  padding: 0.3em 1.2em;
+  color: #333333;
+  background-color: rgba($color: #f6f6f6, $alpha: 0.8);
+  border-radius: 10px;
+}
+</style>
---
+++ error.vue
@@ -0,0 +1,40 @@
+<script setup lang="ts">
+import BackToHome from "./components/BackToHome.vue";
+
+definePageMeta({
+  layout: "default",
+});
+
+defineProps({
+  error: Object,
+});
+</script>
+
+<template>
+  <div class="error">
+    <Header />
+    <HeaderPageIcon />
+    <PageTitle title="ERROR" subTitle="{{ error?.statusCode }}" />
+    <p class="main">お探しのページは見つかりませんでした。</p>
+    <BackToHome />
+  </div>
+</template>
+
+<style lang="scss">
+@use "@/assets/scss/_breakpoint.scss" as *;
+.main {
+  width: 80%;
+  margin: 40px auto;
+  font-family: "Zen Old Mincho", serif;
+  font-size: 32px;
+  color: #333333;
+  margin: 20px auto;
+  text-align: center;
+  @include md {
+    font-size: 24px;
+  }
+  @include sm {
+    font-size: 12px;
+  }
+}
+</style>
