ac85a1ca3cbd625b24bc3cee8227609b11d8d782
shuma-I <rns07k@gmail.com>
2024-03-05 16:10:58 +0900

    nuxt.config.tsにmainをpull

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,26 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+  app: {
+    baseURL: process.env.BASE_URL ? process.env.BASE_URL : "/",
+    head: {
+      meta: [
+        { charset: "utf-8" },
+        { name: "viewport", content: "width=device-width, initial-scale=1" },
+        { hid: "description", name: "description", content: "" },
+        { property: "og:type", content: "website" },
+        { property: "og:site_name", content: "24清陵祭 - 横浜国立大学大学祭" },
+        {
+          property: "keywords",
+          content:
+            "清陵祭,横浜国立大学,大学祭,文化祭,横国,清涼祭,せいりょうさい,2024",
+        },
+        // {property: 'og:image', content: 'localhost:3000/favicon.ico'},
+        // {property: 'og:image:width', content: '1200'},
+        // {property: 'og:image:height', content: '630'},
+      ],
+    },
+  },
   modules: [
     [
       "@nuxtjs/google-fonts",
@@ -10,7 +30,7 @@ export default defineNuxtConfig({
         },
         preconnect: true,
         crossorigin: true,
-        display: 'swap'
+        display: "swap",
       },
     ],
   ],
