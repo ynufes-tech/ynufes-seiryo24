42180cfb6bb8d11213a601defa05119ebf395c57
shuma-I <rns07k@gmail.com>
2024-03-07 22:25:54 +0900

    コンフリクトの解消を試みた

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,9 +1,10 @@
 <script setup lang="ts">
-import { onMounted } from 'vue';
+import { onMounted } from "vue";
 
 onMounted(() => {
-  document.body.style.margin = '0';
-});</script>
+  document.body.style.margin = "0";
+});
+</script>
 <template>
   <div class="layout-root">
     <header-component />
