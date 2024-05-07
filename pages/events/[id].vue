ec06fb23373fef9671ab3eb89728adaf917a554a
Ito Hayato <131941304+hayato19@users.noreply.github.com>
2024-05-07 15:48:00 +0900

    WIP

--- pages/events/[id].vue
+++ pages/events/[id].vue
@@ -56,9 +56,9 @@ const area_id = event?.area ?? 0;
       </div>
       <h1 class="org-name">{{ event?.org_name }}</h1>
       <!-- <p
-                    class="place"
-                    v-text="'企画場所: ' + placeToString(area_id) + event?.place_name ?? ''"
-                  /> -->
+                          class="place"
+                          v-text="'企画場所: ' + placeToString(area_id) + event?.place_name ?? ''"
+                        /> -->
       <img :src="``" class="event-image" />
       <p class="section-title">・企画説明</p>
       <p class="event-description" v-text="event?.event_description" />
@@ -69,19 +69,19 @@ const area_id = event?.area ?? 0;
           class="activity-images"
         >
           <!-- <swiper
-                                v-if="event?.activity_images > 1"
-                                :autoplay="{
-                                  delay: 8000,
-                                  disableOnInteraction: false,
-                                }"
-                                :modules="[Pagination, Autoplay]"
-                                :pagination="{
-                                  dynamicBullets: true,
-                                }"
-                                class="activity-images-swiper"
-                              >
-                                <swiper-slide v-for="url in urls"><img :src="url" /></swiper-slide>
-                              </swiper> -->
+                                          v-if="event?.activity_images > 1"
+                                          :autoplay="{
+                                            delay: 8000,
+                                            disableOnInteraction: false,
+                                          }"
+                                          :modules="[Pagination, Autoplay]"
+                                          :pagination="{
+                                            dynamicBullets: true,
+                                          }"
+                                          class="activity-images-swiper"
+                                        >
+                                          <swiper-slide v-for="url in urls"><img :src="url" /></swiper-slide>
+                                        </swiper> -->
           <img
             v-if="event?.activity_images == 1"
             :src="urls[0]"
