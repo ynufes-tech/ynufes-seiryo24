da6d9b00a5d0d4c9409e940f44991c3e1c00b4f0
Ito Hayato <131941304+hayato19@users.noreply.github.com>
2024-05-08 11:30:05 +0900

    SwiperElementの導入 バナー画像にswiperを適量

--- components/SponsorBanner.vue
+++ components/SponsorBanner.vue
@@ -1,28 +1,40 @@
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import { register } from "swiper/element/bundle";
+
+register();
+</script>
 
 <template>
   <div class="sponsor-banner">
-    <a href="" target="_blank">
-      <img alt="" src="../assets/images/banner-images/banner-image1.webp" />
-    </a>
-    <a href="" target="_blank">
-      <img alt="" src="../assets/images/banner-images/banner-image2.webp" />
-    </a>
+    <swiper-container
+      loop="true"
+      css-mode="true"
+      :autoplay="{ delay: 3000, disableOnInteraction: false }"
+    >
+      <swiper-slide>
+        <img alt="" src="../assets/images/banner-images/banner-image1.webp" />
+      </swiper-slide>
+      <swiper-slide>
+        <img alt="" src="../assets/images/banner-images/banner-image2.webp" />
+      </swiper-slide>
+    </swiper-container>
   </div>
 </template>
 
 <style lang="scss" scoped>
 .sponsor-banner {
   display: flex;
-  flex-wrap: wrap;
-  justify-content: space-between;
-  width: 70%;
+  justify-content: center;
+  width: 100%;
 
-  a {
-    margin: 15px 25px;
+  swiper-container {
+    width: 50svw;
+    height: 100%;
+    padding-top: 4svw;
 
     img {
-      object-fit: contain;
+      width: 100%;
+      height: 100%;
     }
   }
 }
