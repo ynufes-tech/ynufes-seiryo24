7e3b360b36f516b108328b53e522b7fbb040c27d
hinata0629 <hinata749890@icloud.com>
2024-05-08 18:57:39 +0900

    page完成

--- components/EventWrap.vue
+++ components/EventWrap.vue
@@ -31,26 +31,52 @@ const props = defineProps({
 <style lang="scss" scoped>
 @use "@/assets/scss/_breakpoint.scss" as *;
 .event-wrap {
-  height: 420px;
-  width: 420px;
+  height: 460px;
+  width: 460px;
   border-radius: 50%;
   background-color: rgba(#f6f6f6, 0.8);
   text-align: center;
+  @include md {
+    height: 380px;
+    width: 380px;
+  }
+  @include sm {
+    height: 300px;
+    width: 300px;
+  }
   > h1 {
     margin: 20px auto;
-    font-size: 24px;
-    padding-top: 65px;
-    width: 320px;
+    font-size: 20px;
+    padding-top: 70px;
+    width: 460px;
     @include md {
-      font-size: 20px;
+      font-size: 16px;
+      width: 380px;
+      padding-top: 60px;
+      margin: 15px auto;
+    }
+    @include sm {
+      width: 300px;
+      font-size: 12px;
+      padding-top: 50px;
+      margin: 12px auto;
     }
   }
   > p {
     margin: 15px auto;
     font-size: 16px;
-    width: 320px;
+    width: 380px;
+    padding: 0 30px;
+    white-space: pre-wrap;
     @include md {
-      font-size: 12px;
+      font-size: 14px;
+      width: 340px;
+      padding: 0 20px;
+    }
+    @include sm {
+      width: 270px;
+      font-size: 10px;
+      padding: 0 15px;
     }
   }
 }
